f2c29895ecf4242bb34c5ed3a64d68c71af529e1
MoidulHasan <antu.khan.988@gmail.com>
2023-09-10 13:46:49 +0600

    error page created

---
+++ website/src/error.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+interface ErrorProps {
+  error: {
+    description?: string;
+    message?: string;
+    statusCode: string;
+    statusMessage: string;
+    url: string;
+  };
+}
+
+const { error } = defineProps<ErrorProps>();
+
+const component = computed(() => {
+  return parseInt(error.statusCode) === 404
+    ? defineAsyncComponent(
+        () => import("~/components/Layouts/error/NotFound.vue"),
+      )
+    : defineAsyncComponent(
+        () => import("~/components/layouts/error/ServerError.vue"),
+      );
+});
+
+definePageMeta({
+  layout: "empty",
+  title: "Error",
+});
+</script>
+
+<template>
+  <div>
+    <Head>
+      <Title>{{ error.statusMessage }}</Title>
+    </Head>
+
+    <Component :is="component" :error="error" />
+  </div>
+</template>
