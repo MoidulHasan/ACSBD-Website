e908fb9c3a1331e0a1545a34ec53bb6fe8f5cd08
MoidulHasan <antu.khan.988@gmail.com>
2023-09-10 16:40:10 +0600

    container class updated

--- website/src/components/Pages/Home/CustomerReviewSection.vue
+++ website/src/components/Pages/Home/CustomerReviewSection.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
+  <div class="container">
     <CommonSliderSection>
       <template #header>
-        <h1>Our Customers Say</h1>
+        <h1 class="heading2">Our Customers Say</h1>
+        <p>
+          We have already completed 3500+ projects. Our clients are Hospitals,
+          Group of companies, Markets, Bank, Shopping Mall, Multinational
+          Organization, University, Governments project, Tender Work, private
+          business sector, House building and so on. Here are some list of
+          customers.
+        </p>
       </template>
     </CommonSliderSection>
   </div>
