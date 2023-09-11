7dcb40869097f58fb3f6213267f95051eabec9bd
Najim473 <najim7304@gmail.com>
2023-09-11 17:09:06 +0600

    section structure complete

--- website/src/components/Pages/Home/AtaGlance.vue
+++ website/src/components/Pages/Home/AtaGlance.vue
@@ -1,21 +1,50 @@
 <template>
-  <div>
-    <h3>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-      obcaecati accusamus expedita sequi commodi reiciendis velit autem
-      cupiditate fugiat sit, suscipit, quod quisquam corrupti quae consectetur.
-      Molestias aliquid sunt sint nam, delectus earum nemo cupiditate, natus
-      veritatis ut perspiciatis eius, atque temporibus excepturi magnam quod
-      perferendis harum at. Sed doloremque dolorem repellendus commodi iure.
-      Consectetur suscipit provident saepe, a illo ipsam, ipsa officia tempore
-      mollitia placeat laboriosam voluptate, ut et iste quisquam sed inventore
-      eligendi dolore reiciendis est aperiam perferendis laudantium quod
-      laborum. Saepe velit minus voluptatem necessitatibus nihil maxime
-      consequatur cupiditate ex veniam libero, tempora natus voluptatibus beatae
-      aliquam molestias totam veritatis quis commodi! Incidunt magnam quibusdam
-      dignissimos nostrum iure cumque ipsam similique tempora! Nulla, labore?
-      Delectus, adipisci eligendi?
-    </h3>
+  <div class="at-a-glance">
+    <div class="bg-glance">
+      <div class="container-glance container">
+        <h2>AC Service at a Glance</h2>
+        <div class="root-glance grid">
+          <div class="service-glance col">
+            <div class="icon-glance">
+              <img src="/images/expert-technician.svg" alt="expert" />
+            </div>
+            <div class="count-glance">
+              <span>45+</span>
+              <p>Expert Technician</p>
+            </div>
+          </div>
+          <div class="service-glance col">
+            <div class="icon-glance">
+              <img src="/images/complete-projects.svg" alt="expert" />
+            </div>
+            <div class="count-glance">
+              <span>25000+</span>
+              <p>Completed Projects</p>
+            </div>
+          </div>
+
+          <div class="service-glance col">
+            <div class="icon-glance">
+              <img src="/images/happy-customers.svg" alt="expert" />
+            </div>
+            <div class="count-glance">
+              <span>24600+</span>
+              <p>Happy Customers</p>
+            </div>
+          </div>
+
+          <div class="service-year-glance col">
+            <div class="icon-glance">
+              <img src="/images/years-of-company.svg" alt="expert" />
+            </div>
+            <div class="count-glance">
+              <span>16+</span>
+              <p>Years of Company</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -23,4 +52,127 @@ export default {
   name: "AtaGlance",
 };
 </script>
-<style lang=""></style>
+<style lang="scss">
+.at-a-glance {
+  color: var(--primary-color-white);
+  background-image: url("/images/at-a-glance.png");
+  background-size: cover;
+  text-align: center;
+  height: 300px;
+  width: 100%;
+  .bg-glance {
+    width: 100%;
+    height: 100%;
+    background: rgba(29, 53, 102, 0.8);
+    .container-glance {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-items: center;
+      height: 100%;
+      h2 {
+        text-align: center;
+        font-size: 28px;
+        margin-top: 45px;
+      }
+      .root-glance {
+        width: 100%;
+        margin-top: 0px;
+        .service-glance {
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          .icon-glance {
+            height: 50px;
+            width: 50px;
+            background: var(--primary-color-white);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 50%;
+            img {
+              width: 25px;
+              height: 25px;
+            }
+          }
+          .count-glance {
+            margin-top: 5px;
+            & span {
+              font-size: 25px;
+              font-weight: 600;
+            }
+            p {
+              margin-top: 0px;
+              font-size: 15px;
+              font-weight: 500;
+              letter-spacing: 0.2px;
+            }
+          }
+          &::before {
+            content: "";
+            position: absolute;
+            width: 1.2px;
+            height: 50%;
+            background: linear-gradient(
+              to top,
+              #ffffff,
+              rgba(29, 53, 102, 0.1)
+            );
+            top: 0;
+            left: 3;
+            right: 0;
+            /* bottom: 0; */
+          }
+          &::after {
+            content: "";
+            position: absolute;
+            width: 1.2px;
+            height: 50%;
+            background: linear-gradient(
+              to bottom,
+              #ffffff,
+              rgba(29, 53, 102, 0.1)
+            );
+            left: 3;
+            right: 0;
+            bottom: 0;
+          }
+        }
+        .service-year-glance {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          .icon-glance {
+            height: 50px;
+            width: 50px;
+            background: var(--primary-color-white);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 50%;
+            img {
+              width: 25px;
+              height: 25px;
+            }
+          }
+          .count-glance {
+            margin-top: 5px;
+            & span {
+              font-size: 25px;
+              font-weight: 600;
+            }
+            p {
+              margin-top: 0px;
+              font-size: 15px;
+              font-weight: 500;
+              letter-spacing: 0.2px;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+ss
--- website/src/pages/index.vue
+++ website/src/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <PagesHomeCustomerReviewSection />
+    <PagesHomeAtaGlance />
   </div>
 </template>
 
