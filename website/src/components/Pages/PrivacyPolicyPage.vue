da69b59de8810f6a9f58e09524bbcac2d6f36a14
shamim392 <shaminur392@gmail.com>
2023-09-13 01:22:59 +0600

    content add on page

--- website/src/components/Pages/PrivacyPolicyPage.vue
+++ website/src/components/Pages/PrivacyPolicyPage.vue
@@ -1,6 +1,108 @@
 <template>
-  <div>
-    <div class="text-3xl font-bold underline">this is privacy policy page</div>
+  <div class="container">
+    <div>
+      <h1 class="heading2 mb-0">Privacy Policy</h1>
+      <p class="subtitle text-left pt-3">
+        We have already completed 3500+ projects. Our clients are Hospitals,
+        Group of companies, Markets, Bank, Shopping Mall, Multinational
+        Organization, University, Governments project, Tender Work, private
+        business sector, House building and so on. Here are some list of
+        customers.
+      </p>
+    </div>
+    <div>
+      <h1 class="heading3 mb-0 pt-6">Data that we collect</h1>
+      <p class="subtitle text-left pt-3">
+        We may collect various pieces of information if you seek to place an
+        order for a product with us on the Site. We collect, store and process
+        your data for processing your purchase on the Site and any possible
+        later claims, and to provide you with our services. We may collect
+        personal information including, but not limited to, your title, name,
+        gender, date of birth, email address, postal address, delivery address
+        (if different), telephone number, mobile number, fax number, payment
+        details, payment card details or bank account details. We will use the
+        information you provide to enable us to process your orders swiftly.
+      </p>
+    </div>
+    <div>
+      <h1 class="heading3 mb-0 pt-6">
+        Purposes for collection, Storage and/or use of personal data or
+        information
+      </h1>
+      <p class="subtitle text-left pt-3">
+        We may collect various pieces of information if you seek to place an
+        order for a product with us on the Site. We collect, store and process
+        your data for processing your purchase on the Site and any possible
+        later claims, and to provide you with our services. We may collect
+        personal information including, but not limited to, your title, name,
+        gender, date of birth, email address, postal address, delivery address
+        (if different), telephone number, mobile number, fax number, payment
+        details, payment card details or bank account details. We will use the
+        information you provide to enable us to process your orders swiftly and
+        to provide you with the services and information offered through our
+        website and which you request. <br />
+        <br />
+        We may pass your name and address on to a third party in order to make
+        delivery of the product to you (for example to our courier or supplier).
+        You must only submit to us the Site information which is accurate and
+        not misleading and you must keep it up to date and inform us of changes.
+      </p>
+    </div>
+    <div>
+      <h1 class="heading3 mb-0 pt-6">Data that we cllect</h1>
+      <p class="subtitle text-left pt-3">
+        We may collect various pieces of information if you seek to place an
+        order for a product with us on the Site. We collect, store and process
+        your data for processing your purchase on the Site and any possible
+        later claims, and to provide you with our services. We may collect
+        personal information including, but not limited to, your title, name,
+        gender, date of birth, email address, postal address, delivery address
+        (if different), telephone number, mobile number, fax number, payment
+        details, payment card details or bank account details. We will use the
+        information you provide to enable us to process your orders swiftly and
+        to provide you with the services and information offered through our
+        website and which you request. Further, we will use the information you
+        provide to administer your account with us; verify and carry out
+        financial transactions in relation to payments you make; audit the
+        downloading of data from our website; improve the layout and/or content
+        of the pages of our website and customize them for users; identify
+        visitors on our website; carry out research on our users’ demographics.
+      </p>
+    </div>
+    <div>
+      <h1 class="heading3 mb-0 pt-6">Third parties and Links</h1>
+      <p class="subtitle text-left pt-3">
+        We may pass your details to other companies in our group. We may also
+        pass your details to our agents and subcontractors to help us with any
+        of our uses of your data set out in our Privacy Policy. For example, we
+        may use third parties to assist us with delivering products to you, to
+        help us to collect payments from you, to analyze data and to provide us
+        with marketing or customer service assistance.
+      </p>
+    </div>
+    <div>
+      <h1 class="heading3 mb-0 pt-6">Security</h1>
+      <p class="subtitle text-left pt-3">
+        We have in place appropriate technical and security measures to prevent
+        unauthorized or unlawful access to or accidental loss of or destruction
+        or damage to your information. When we collect data through the Site, we
+        collect your personal details on a secure server. We use firewalls on
+        our servers. Our security procedures mean that we may occasionally
+        request proof of identity before we disclose personal information to
+        you. You are responsible for protecting against unauthorized access to
+        your password and to your computer.
+      </p>
+    </div>
+    <div>
+      <h1 class="heading3 mb-0 pt-6">Your Rights</h1>
+      <p class="subtitle text-left pt-3 pb-8">
+        If you are concerned about your data you have the right to request
+        access to the personal data which we may hold or process about you. You
+        have the right to require us to correct any inaccuracies in your data
+        free of charge. At any stage you also have the right to ask us to stop
+        using your personal data for direct marketing purposes.
+      </p>
+    </div>
   </div>
 </template>
 
