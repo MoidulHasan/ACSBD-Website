b5790430d0c859b9998477dc8d43862a66d56a67
MoidulHasan <antu.khan.988@gmail.com>
2023-09-13 09:25:16 +0600

    feat: client review page created

---
+++ website/src/pages/client-review.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container flex">
+    <CommonClientReviewCard
+      v-for="(clientReview, index) in clientReviews"
+      :key="'cleint-review-' + index"
+      :image-url="clientReview.imageUrl"
+      :name="clientReview.name"
+      :review="clientReview.review"
+      :title="clientReview.title"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { getCoustomersReview } from "~/app/api/customers-review";
+
+const {
+  data: clientReviews,
+  pending,
+  error,
+  refresh,
+} = await getCoustomersReview();
+</script>
+
+<style scoped></style>
