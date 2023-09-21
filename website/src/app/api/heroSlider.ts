ea465ad5bb238adb2ca16950f19533daa416bbc8
MoidulHasan <antu.khan.988@gmail.com>
2023-09-21 23:06:04 +0600

    refactor: hero slider api fetching method refactored

--- website/src/app/api/heroSlider.ts
+++ website/src/app/api/heroSlider.ts
@@ -2,8 +2,8 @@ export const getHeroSliderData = async () => {
   const runtimeConfig = useRuntimeConfig();
 
   const { data, pending, error, refresh } = await useAsyncData(
-    "customers-review",
-    () => $fetch(runtimeConfig.apiUrl + "/data/hero-slider-data.json")
+    "hero-slider-content",
+    () => $fetch(runtimeConfig.apiUrl + "/data/hero-slider-data.json"),
   );
 
   return { data, pending, error, refresh };
