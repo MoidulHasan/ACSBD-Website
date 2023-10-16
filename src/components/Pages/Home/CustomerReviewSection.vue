5a87e0f32d39f128f25abf2b65881874e1dc1705
MoidulHasan <antu.khan.988@gmail.com>
2023-10-17 00:55:06 +0600

    refactor: style issues refactored

--- src/components/Pages/Home/CustomerReviewSection.vue
+++ src/components/Pages/Home/CustomerReviewSection.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="h-screen">
-    <CommonSliderSection
-      :items="clientReviews"
-      :number-of-scroll="1"
-      :visible-item="4"
-      slide-component="ClientReviewCard"
-    >
-      <template #header>
-        <CommonSectionHeader
-          header="Our Customers Say"
-          sub-header="We have already completed 3500+ projects. Our clients are Hospitals,
+  <CommonSliderSection
+    :items="clientReviews"
+    :number-of-scroll="1"
+    :visible-item="4"
+    slide-component="ClientReviewCard"
+  >
+    <template #header>
+      <CommonSectionHeader
+        header="Our Customers Say"
+        sub-header="We have already completed 3500+ projects. Our clients are Hospitals,
                   Group of companies, Markets, Bank, Shopping Mall, Multinational
                   Organization, University, Governments project, Tender Work, private
                   business sector, House building and so on. Here are some list of
