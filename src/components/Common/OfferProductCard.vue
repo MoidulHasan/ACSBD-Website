57458b6e74b9c00aea3c6a1d4589bec86622a396
MoidulHasan <antu.khan.988@gmail.com>
2023-10-17 02:22:37 +0600

    refactor: styles refactored

--- src/components/Common/OfferProductCard.vue
+++ src/components/Common/OfferProductCard.vue
@@ -6,6 +6,7 @@
 
     <div class="product-info flex flex-column">
       <p class="text-color-primary-dark-gray text-semi-bold-1">
+      <p class="text-color-primary-dark-gray text-semi-bold-1">
         Black 1.5 Ton GMCC Rotary Compressor
       </p>
 
@@ -30,15 +31,15 @@
           <span>{{ rating }}</span>
         </div>
 
-        <div
-          class="review text-color-primary-dark-gray text-center text-regular-3"
-        >
+        <div class="review text-color-primary-dark-gray text-center text-regular-3">
           {{ total_review }} Reviews
         </div>
       </div>
 
       <hr class="divider" />
 
+      <p class="text-semi-bold-1 text-color-primary-dark-gray">Hurry UP !</p>
+      <p class="text-regular-4 text-color-primary-dark-gray">Offers end in:</p>
       <p class="text-semi-bold-1 text-color-primary-dark-gray">Hurry UP !</p>
       <p class="text-regular-4 text-color-primary-dark-gray">Offers end in:</p>
 
