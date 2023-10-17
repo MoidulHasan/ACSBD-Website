f3ba83ad83346371cac04c66d18d9b9bdc03a1c9
hasan75 <hasanjab14@gmail.com>
2023-10-17 23:20:24 +0600

    feature(welcome): design of welcome page completed

--- src/pages/welcome.vue
+++ src/pages/welcome.vue
@@ -4,7 +4,7 @@
       <h2 class="form-title">Welcome!</h2>
       <p class="form-sub-title">Signup or login to continue</p>
       <div class="button-container mt-5">
-        <Button class="mb-4 button-primary auth-button login">Log In</Button>
+        <Button class="mb-4 auth-button login">Log In</Button>
         <Button class="auth-button signup">Sign Up</Button>
       </div>
       <div class="or-container my-4">
@@ -12,6 +12,26 @@
         <p class="or-text">Or</p>
         <div class="line-through"></div>
       </div>
+      <div class="button-container mb-4">
+        <Button class="mb-4 auth-button alternative">
+          <img src="~/assets/images/auth/apple.svg" alt="Apple" />
+          <span class="button-text">Continue with Apple</span>
+        </Button>
+        <Button class="mb-4 auth-button alternative">
+          <img src="~/assets/images/auth/google.svg" alt="Google" />
+          <span class="button-text">Continue with Google</span>
+        </Button>
+        <Button class="auth-button alternative">
+          <img src="~/assets/images/auth/facebook.svg" alt="Facebook" />
+          <span class="button-text">Continue with Facebook</span>
+        </Button>
+      </div>
+      <ul class="sign-content">
+        <li class="sign-text">
+          By signing up, you agree to our Terms of Use and to receive Jicson
+          emails & updates and acknowledge that you read our Privacy Policy.
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -45,6 +65,11 @@ definePageMeta({
     z-index: -3;
     opacity: 0.5;
   }
+  @media (max-width: 900px) {
+    background-image: none;
+    background-color: var(--primary-color-white);
+    opacity: 1;
+  }
 
   .form-content {
     padding: 40px;
@@ -69,7 +94,6 @@ definePageMeta({
       font-weight: 400;
       line-height: normal;
     }
-
     .button-container {
       display: flex;
       flex-direction: column;
@@ -82,6 +106,7 @@ definePageMeta({
         justify-content: center;
         align-items: center;
         box-shadow: none;
+        gap: 12px;
       }
       .login {
         background: var(--primary-color-envitect-sam-blue);
@@ -92,8 +117,19 @@ definePageMeta({
         background: var(--primary-color-white);
         border: 1px solid var(--primary-color-envitect-sam-blue) !important;
       }
-    }
+      .alternative {
+        border: 1px solid #dcdae8;
+        background: #fff;
 
+        .button-text {
+          color: #808080;
+          font-size: 18px;
+          font-style: normal;
+          font-weight: 500;
+          line-height: normal;
+        }
+      }
+    }
     .or-container {
       display: flex;
       justify-content: center;
@@ -116,9 +152,28 @@ definePageMeta({
         content: " ";
         text-shadow: none;
         width: 100px;
-        background: #DCDAE8;
+        background: #dcdae8;
       }
+    }
+    .sign-content {
+      padding-inline-start: 20px;
+      .sign-text {
+        max-width: 460px;
+        text-align: justify;
+        color: #808080;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 24px;
+      }
+    }
+
+    @media (max-width: 900px) {
+      padding: 2rem 1rem;
+      min-width: 328px;
+      margin: 2rem 1rem;
     }
   }
 }
+
 </style>
