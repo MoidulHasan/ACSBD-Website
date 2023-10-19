7ebca16bb9c24a6c2295f9f1b62d64b042c4db6b
MoidulHasan <antu.khan.988@gmail.com>
2023-10-20 01:43:47 +0600

    feat: design system implementation completed and old css replaced in some place

--- src/components/Common/ProductCard.vue
+++ src/components/Common/ProductCard.vue
@@ -30,7 +30,17 @@
 </template>
 
 <script lang="ts" setup>
-import { Product } from "~/contracts/common";
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  discount_price: number;
+  discount_percentage: number;
+  rating: number;
+  total_review: number;
+  image: string;
+  offerEndsAt: string;
+}
 
 const props = defineProps<Product>();
 
