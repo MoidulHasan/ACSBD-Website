6d8f0b2511d1e2af0379179df5d389b86dfa94a7
MoidulHasan <antu.khan.988@gmail.com>
2023-10-23 02:16:37 +0600

    refactor: faq page refactored

--- src/pages/frequently-asked-questions.vue
+++ src/pages/frequently-asked-questions.vue
@@ -1,13 +1,48 @@
 <template>
-  <div>
-    <PagesFaqTopTitle />
-    <PagesFaqTabsSection />
+  <div class="flex flex-column align-items-center">
+    <div class="container">
+      <PagesFaqTopTitle class="my-48px" />
+      <PagesFaqTabsSection
+        :active-tab-index="activeTabIndex"
+        :faq-tabs="faqTabData"
+        class="mb-48px py-5"
+        @on-active-tab-change="handleActiveTabChange"
+      />
+    </div>
+
+    <PagesFaqTabComponent :faq-list="faqListOfSelectedTab" />
     <PagesFaqMoreQuestionForm />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { getFAQData } from "~/app/api/getFAQData";
+import type { FAQSection } from "~/contracts/api-contracts/faqData";
+
 definePageMeta({
   title: "Frequently Asked Questions",
 });
+
+const { data: allFaqData } = await getFAQData();
+
+const faqTabData = computed(() => {
+  if (!allFaqData.value) return [];
+
+  return allFaqData.value.map((faqData: FAQSection) => {
+    return {
+      title: faqData.title,
+      image: faqData.image,
+      description: faqData.description,
+    };
+  });
+});
+
+const faqListOfSelectedTab: Ref<FAQSection> = computed(() => {
+  return allFaqData.value?.length ? allFaqData.value[activeTabIndex.value] : [];
+});
+
+const activeTabIndex = ref(0);
+const handleActiveTabChange = (index: number) => {
+  activeTabIndex.value = index;
+};
 </script>
