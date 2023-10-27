6cbfcfbe0c7b4e1f41fbd543827f77a0614e1058
MoidulHasan <antu.khan.988@gmail.com>
2023-10-27 22:25:08 +0600

    refactor: useAsyncFetch replaced with useFetch

--- src/app/api/getHomePageHeroSliderImages.ts
+++ src/app/api/getHomePageHeroSliderImages.ts
@@ -1,12 +1,8 @@
 export const getHomePageHeroSliderImages = async () => {
   const runtimeConfig = useRuntimeConfig();
 
-  const { data, pending, error, refresh } = await useAsyncData(
-    "home-page-hero-slider-images",
-    () =>
-      $fetch(
-        runtimeConfig.public.apiUrl + "/data/home-page-hero-slider-images.json",
-      ),
+  const { data, pending, error, refresh } = await useFetch(
+    runtimeConfig.public.apiUrl + "/data/home-page-hero-slider-images.json",
   );
 
   return { data, pending, error, refresh };
