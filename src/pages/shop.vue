80fcd09a476f2e5998f676add61c5ae26c52f7d6
MoidulHasan <antu.khan.988@gmail.com>
2023-10-27 09:11:35 +0600

    feat: products data created and fetching method created

--- src/pages/shop.vue
+++ src/pages/shop.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>
-    <CommonComingSoon />
+  <div class="container grid">
+    <div class="col-0 lg:col-2">filters</div>
+
+    <div class="col-12 lg:col-10">
+      <div>
+        <img alt="AC Service BD Products" src="/images/shop-page-banner.jpeg" />
+      </div>
+
+      <div>
+        {{ products }}
+
+        <!--      <div>-->
+        <!--        <div>-->
+        <!--          <div v-for="product in products" :key="'product-' + product.id">-->
+        <!--            {{ product }}-->
+        <!--          </div>-->
+        <!--        </div>-->
+        <!--      </div>-->
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { getProducts } from "~/app/api/getProducts";
+
 definePageMeta({
   title: "Shop",
 });
+
+const { data: products } = await getProducts();
 </script>
