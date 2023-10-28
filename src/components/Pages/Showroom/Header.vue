449d48dddaf838b4f87e2e2cf329ece41e52417a
Najim473 <najim7304@gmail.com>
2023-10-28 16:21:10 +0600

    showroom page designing

---
+++ src/components/Pages/Showroom/Header.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi- word-component-names -->
+<template>
+  <CommonSectionHeader
+    class="mb-32px"
+    header="Our Showroom and Service Center Locations"
+    sub-header="Your visit to our Istanbul Showroom not only would do us great honour; but you would also have the privilige to have a closer look at our wide range of collection of beautiful fabrics. You can enjoy the beauties of Istanbul for the rest of the day!."
+  />
+</template>
+<script lang="ts" setup></script>
+<style lang="scss" scoped></style>
---
+++ src/components/Pages/Showroom/location.vue
@@ -0,0 +1,6 @@
+// eslint-disable-next-line vue/multi-word-component-names
+<template>
+  <div>This is showroom Location</div>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss" scoped></style>
