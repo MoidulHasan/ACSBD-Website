92ca93f70ca370983821705904b1eb95898f0842
Najim473 <najim7304@gmail.com>
2023-10-28 16:38:45 +0600

    added map

--- src/components/Pages/Showroom/location.vue
+++ src/components/Pages/Showroom/location.vue
@@ -1,6 +1,21 @@
 // eslint-disable-next-line vue/multi-word-component-names
 <template>
-  <div>This is showroom Location</div>
+  <div class="container grid w-full mb-5">
+    <div class="h-20rem bg-primary col-6">
+
+        
+    </div>
+    <div class="h-20rem bg-black-alpha-60 col-6">
+      <iframe
+        class="w-full h-full border-round-sm"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14594.146117743501!2d90.40015865!3d23.870586149999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1698489167226!5m2!1sen!2sbd"
+        style="border: 0"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  </div>
 </template>
 <script lang="ts" setup></script>
 <style lang="scss" scoped></style>
