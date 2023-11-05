39417b7335e3efb918c6669c1f88b0762aa1b87f
MoidulHasan <antu.khan.988@gmail.com>
2023-11-05 16:46:17 +0600

    feat: useBreadCrumbs composable created and breadcrumb component created

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,6 +3,9 @@
     <div>
       <LayoutsHeaderNav />
     </div>
+    <div v-if="!hideBreadCrumb">
+      <LayoutsBreadCrumb :breadcrumbs="breadcrumbs" />
+    </div>
 
     <div>
       <slot />
@@ -14,6 +17,16 @@
   </main>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { width } = useWindowSize();
+
+const { breadcrumbs } = useBreadcrumbs();
+
+const router = useRouter();
+
+const hideBreadCrumb = computed(() => {
+  return router.currentRoute.value.fullPath === "/" || width.value <= 768;
+});
+</script>
 
 <style scoped></style>
