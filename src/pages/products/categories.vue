f5c55bec3fb89ec4c3896843ddab1a44d6c23c2a
hasan75 <hasanjab14@gmail.com>
2023-11-07 01:31:14 +0600

    category page title font size reduced and image size reduced for mobile

--- src/pages/products/categories.vue
+++ src/pages/products/categories.vue
@@ -12,13 +12,13 @@ const { data: categories } = await getAcCategories();
 <template>
   <div class="container category-container">
     <div
-      class="category-flex-container flex flex-wrap py-3 align-items-center justify-content-center lg:justify-content-start"
+      class="category-flex-container flex flex-wrap pt-3 pb-4 align-items-center justify-content-center lg:justify-content-start"
     >
       <div
         v-for="category in categories"
         class="single-category bg-color-product-bg text-center flex flex-column justify-content-around flex-wrap"
       >
-        <img :src="category.img" :alt="category.title" />
+        <img class="category-image" :src="category.img" :alt="category.title" />
         <h3 class="category-title text-semi-bold-5 text-dark-gray-80 mt-10px">
           {{ category.title }}
         </h3>
@@ -28,16 +28,48 @@ const { data: categories } = await getAcCategories();
 </template>
 
 <style lang="scss" scoped>
+@use "assets/styles/scss/base/mixins" as *;
 .category-container {
   min-height: 30rem;
+  @include media-query(md) {
+    min-height: 20rem;
+  }
+  @include media-query(sm) {
+    min-height: 11rem;
+  }
   .category-flex-container {
     gap: 15px;
+
+    @include media-query(sm) {
+      gap: 8px;
+    }
   }
   .single-category {
     width: 227px;
     height: 140px;
-    padding: 12px 28px;
     border-radius: 4px;
+    @include media-query(sm) {
+      border-radius: 2px;
+      width: 104px;
+      height: 64px;
+
+
+    }
+  }
+  .category-image {
+    @include media-query(sm) {
+      max-width: 84.789px;
+      max-height: 39.2px;
+    }
+  }
+  .category-title {
+    @include media-query(sm) {
+      text-align: center;
+      font-size: 8px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 12px;
+    }
   }
 }
 </style>
