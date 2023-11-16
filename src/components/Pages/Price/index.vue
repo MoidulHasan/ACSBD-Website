8f329cd55eb90aca9ce40db546699fa1e38d6c00
Najim473 <najim7304@gmail.com>
2023-11-16 23:41:26 +0600

    fet 17-develope-pricing-page customized table design

--- src/components/Pages/Price/index.vue
+++ src/components/Pages/Price/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h2 class="text-center py-32px bg-dark-gray-10 text-4xl font-medium opacity-80">AC Installation</h2>
     <DataTable :value="productsPrices" expended show-gridlines >
       <Column header="S.l" headerStyle="width:3rem">
         <template #body="slotProps">
@@ -35,4 +36,11 @@ const columns = ref([
 console.log(columns)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+:deep .p-column-header-content {
+  font-size: 20px;
+}
+:deep .p-datatable.p-datatable-gridlines:has(.p-datatable-thead):has(.p-datatable-tbody) .p-datatable-tbody > tr > td:last-child {
+font-weight:700;
+}
+</style>
