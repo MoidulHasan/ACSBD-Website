c0ab6adf9f0672b3ea5925343d5b35857a86632f
hasan75 <hasanjab14@gmail.com>
2023-12-29 20:52:28 +0600

    Blog Search and sidebar construction added

---
+++ src/components/Common/Search.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+  placeholder: string;
+}>();
+const emit = defineEmits<{
+  (e: "search", value: string): void;
+}>();
+const searchText = ref("");
+const submitText = () => {
+  emit("search", searchText.value);
+};
+</script>
+<template>
+  <div class="p-inputgroup flex-1 flex-wrap w-full">
+    <InputText v-model="searchText" :placeholder="placeholder" />
+    <Button class="search-button" icon="pi pi-search" @click="submitText" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.search-button,
+.search-button:hover {
+  background-color: var(--primary-color-envitect-sam-blue);
+  padding: 12px 29px;
+}
+
+:deep(.p-inputgroup .p-inputtext) {
+  max-height: 40px;
+}
+:deep(.p-inputgroup .p-button) {
+  max-height: 40px;
+}
+</style>
--- src/components/Pages/Blogs/BlogSideBarOptions.vue
+++ src/components/Pages/Blogs/BlogSideBarOptions.vue
@@ -1,7 +1,37 @@
 <template>
-  <div class="p-3 lg:py-3 lg:px-0">This is sidebar</div>
+  <div class="">
+    <CommonSearch
+      placeholder="Search blogs here"
+      class="mb-3"
+      @search="handleSearchResult"
+    />
+    <div class="all-category-block p-3 bg-color-product-bg">
+      <h3 class="pb-3 text-semi-bold-1 text-primary-color-navy-blue">
+        All Categories
+      </h3>
+      <div class="flex category-list">
+        <p
+          class="py-4px px-16px bg-envitect-sam-blue-2 text-dark-gray-80 text-regular-4"
+        >
+          Ac Category
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const handleSearchResult = (value: string) => {
+  console.log(value);
+};
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.all-category-block {
+  border-radius: 4px;
+
+  .category-list {
+    gap: 12px;
+  }
+}
+</style>
