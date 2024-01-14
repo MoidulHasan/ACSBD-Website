eb00a12a00ff892acad9de2ad2b360ddaf4bc9f9
hasan75 <hasanjab14@gmail.com>
2024-01-14 20:51:24 +0600

    :twisted_rightwards_arrows: Structured single work page with banner and top info

--- src/pages/recent-works/[id].vue
+++ src/pages/recent-works/[id].vue
@@ -14,6 +14,13 @@ const recentWork = ref<RecentWork>({});
 recentWork.value = recentWorks.value?.find(
   (work) => work.id === Number(route.params.id),
 );
+
+const formattedDate = (date: string) => {
+  const dateString = new Date(date);
+  const options = { day: "numeric", month: "long", year: "numeric" };
+  const formattedDate = dateString.toLocaleDateString("en-GB", options);
+  return formattedDate;
+};
 </script>
 
 <template>
@@ -22,7 +29,7 @@ recentWork.value = recentWorks.value?.find(
       :header="recentWork.company"
       :sub-header="recentWork.brief"
     />
-    <div class="work-summary grid">
+    <div class="work-summary mt-5 grid">
       <div class="col-12 md:col-6 lg:col-8">
         <div class="banner-image w-full">
           <NuxtImg
@@ -33,12 +40,43 @@ recentWork.value = recentWorks.value?.find(
         </div>
       </div>
       <div class="col-12 md:col-6 lg:col-4">
-        <div class="key-details border-round-lg bg-color-product-bg p-4">
+        <div class="key-details border-round-lg bg-color-product-bg p-4 h-full">
           <h2
             class="font-heading-4-semi-bold mb-4 text-primary-color-dark-gray"
           >
             Product Details
           </h2>
+          <div class="key-info mb-5">
+            <h2 class="text-primary-color-dark-gray font-heading-5 pb-2">
+              Date
+            </h2>
+            <p class="text-medium-2 text-dark-gray-80">
+              {{ formattedDate(recentWork.startDate) }} to
+              {{ formattedDate(recentWork.endDate) }}
+            </p>
+          </div>
+          <div class="key-info mb-5">
+            <h2 class="text-primary-color-dark-gray font-heading-5 pb-2">
+              Client
+            </h2>
+            <p class="text-medium-2 text-dark-gray-80">
+              {{ recentWork.company }}
+            </p>
+          </div>
+          <div class="key-info mb-5">
+            <h2 class="text-primary-color-dark-gray font-heading-5 pb-2">
+              Project Type
+            </h2>
+            <p class="text-medium-2 text-dark-gray-80">
+              {{ recentWork.type }}
+            </p>
+          </div>
+          <div class="key-info mb-5">
+            <h2 class="text-primary-color-dark-gray font-heading-5 pb-2">
+              Completed By
+            </h2>
+            <p class="text-medium-2 text-dark-gray-80">AC Service BD</p>
+          </div>
         </div>
       </div>
     </div>
