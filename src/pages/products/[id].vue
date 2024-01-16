ee34d9d8af764c69358c37c16bcbec1e23f68b87
hasan75 <hasanjab14@gmail.com>
2024-01-17 01:43:15 +0600

    :art: Stock and review added
    in single page stock and review added

--- src/pages/products/[id].vue
+++ src/pages/products/[id].vue
@@ -6,20 +6,80 @@ const route = useRoute();
 
 const { data: productsData } = await getProducts();
 
-const singleProductData = computed(() => {
+const singleProductData = computed<ProductI>(() => {
   return productsData.value.find((product) => {
     return product.id === Number(route.params.id);
   });
 });
+
+const showReviewNumbers = (count: number) => {
+  if (count >= 0) {
+    if (count < 10) {
+      return `0${count} Review(s)`;
+    }
+    return `${count} Review(s)`;
+  }
+  return "0 Review";
+};
+const isAvailableProduct = (isInStock: boolean) => {
+  return isInStock ? "In Stock" : "Out of Stock";
+};
 </script>
 
 <template>
-  <div class="container">
-    <div class="grid">
+  <div class="container single-product">
+    <div class="grid mt-3">
       <div class="col-12 lg:col-5">Image Slider</div>
-      <div class="col-12 lg:col-7">App</div>
+      <div class="col-12 lg:col-7">
+        <div class="product-summary">
+          <h1 class="product-title">{{ singleProductData.name }}</h1>
+          <div class="product-stock-rating flex align-items-center flex-wrap">
+            <p
+              class="pr-2 md:pr-5 text-medium-2 text-color-primary-dark-gray lg:pt-1"
+            >
+              Available:
+              <span
+                :class="[
+                  'product-stock',
+                  singleProductData.stock.inStock
+                    ? 'text-color-success'
+                    : 'text-color-danger',
+                ]"
+              >
+                {{ isAvailableProduct(singleProductData.stock.inStock) }}
+              </span>
+            </p>
+            <div class="product-rating flex flex-wrap align-items-center">
+              <Rating
+                v-model="singleProductData.ratings.average"
+                :cancel="false"
+                readonly
+              />
+              <p class="pl-2 lg:pt-1">
+                {{ showReviewNumbers(singleProductData.ratings.count) }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.product-summary {
+  .product-title {
+    margin-bottom: 0.75rem;
+  }
+
+  .product-stock-rating {
+    margin-bottom: 0.75rem;
+  }
+
+  .product-rating {
+    :deep(.p-rating-icon) {
+      color: var(--color-orange);
+    }
+  }
+}
+</style>
