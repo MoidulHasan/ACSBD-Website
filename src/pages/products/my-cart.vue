72d4745c29f2b8ec7dcf18a9dd879ed88f38b7cc
hasan75 <hasanjab14@gmail.com>
2024-01-30 23:20:05 +0600

    :package: Feat: started developing my-cart page

---
+++ src/pages/products/my-cart.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+definePageMeta({
+  title: "My Cart",
+  name: "my-cart",
+});
+</script>
+
+<template>
+  <div class="mt-4 mb-5 lg:mb-8">
+    <h2 class="font-heading-2 text-primary-color-navy-blue mb-4">View Cart</h2>
+    <div class="grid">
+      <div class="col lg:col-8"></div>
+      <div class="col lg:col-4"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
