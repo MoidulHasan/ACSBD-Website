dcd2bdcc8e9a8cf19da0846b4341d2f6db75bac5
hasan75 <hasanjab14@gmail.com>
2024-03-04 21:41:52 +0600

    :package: Feat: work pricing list page developed

--- src/components/Pages/WorkPricing/ChillerAC/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h3>Chiller AC</h3>
-  </div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style lang="scss" scoped></style>
--- src/pages/work-pricing.vue
+++ src/pages/work-pricing.vue
@@ -1,36 +1,36 @@
 <script setup lang="ts">
 import { getPricingLists } from "~/app/api/getPricingLists";
 
-const active = ref(0);
-const selectedACOptions = ref();
-const acOptions = ref([
-  { name: "Split AC" },
-  { name: "VRF AC" },
-  { name: "Chiller AC" },
-]);
-watch(
-  () => selectedACOptions.value,
-  () => {
-    selectedACOptions.value.name === "VRF AC"
-      ? (active.value = 1)
-      : selectedACOptions.value.name === "Chiller AC"
-        ? (active.value = 2)
-        : (active.value = 0);
-  },
-);
+definePageMeta({
+  title: "Work Pricing",
+  name: "work-pricing",
+});
 
 const { data: pricingLists } = await getPricingLists();
 
-const splitAcData = computed(() => {
-  return pricingLists.value.find((acData) => {
-    return acData.name === "Split AC";
-  });
+const active = ref(0);
+const selectedACOptions = ref();
+
+const acOptions = computed(() => {
+  return pricingLists.value.map((item) => ({ name: item.name }));
 });
+
+const acOptionsLastIndex = ref(acOptions.value.length - 1);
+
+watch(
+  () => selectedACOptions.value,
+  () => {
+    const selectedIndex = acOptions.value.findIndex(
+      (option) => option.name === selectedACOptions.value?.name,
+    );
+    active.value = selectedIndex !== -1 ? selectedIndex : 0;
+  },
+);
 </script>
 
 <template>
-  <div class="container">
-    <div class="mt-5 mb-8">
+  <div class="mt-5 mb-8">
+    <div class="container">
       <div class="w-full block md:hidden flex justify-content-center">
         <Dropdown
           v-model="selectedACOptions"
@@ -40,45 +40,31 @@ const splitAcData = computed(() => {
           class="w-full text-primary-color-navy-blue px-16px py-12px bg-primary-50 mb-5 font-medium-2"
         />
       </div>
-
       <div
         class="all-works-tab hidden md:flex align-items-center justify-content-center mb-5"
       >
         <button
-          class="w-235px h-50px text-dark-gray-60 px-80px py-12px font-heading-5-semi-bold border-none cursor-pointer border-round-left"
-          :class="{ 'button-tab': active === 0 }"
-          @click="active = 0"
-        >
-          Split AC
-        </button>
-
-        <button
+          v-for="(acOption, index) in acOptions"
+          :key="acOption.name"
+          :index="index"
           class="w-235px h-50px text-dark-gray-60 px-80px py-12px font-heading-5-semi-bold border-none cursor-pointer"
-          :class="{ 'button-tab': active === 1 }"
-          @click="active = 1"
+          :class="[
+            { 'border-round-left': index === 0 },
+            { 'border-round-right': index === acOptionsLastIndex },
+            { 'button-tab': active === index },
+          ]"
+          @click="active = index"
         >
-          VRF AC
-        </button>
-        <button
-          class="w-235px h-50px text-dark-gray-60 px-80px py-12px font-heading-5-semi-bold border-none cursor-pointer border-round-right"
-          :class="{ 'button-tab': active === 2 }"
-          @click="active = 2"
-        >
-          Chiller AC
+          {{ acOption.name }}
         </button>
       </div>
       <TabView v-model:activeIndex="active">
-        <TabPanel>
-          <PagesWorkPricingSplitAC :ac-data="splitAcData" />
-        </TabPanel>
-        <TabPanel>
-          <PagesWorkPricingVRFAC />
-        </TabPanel>
-        <TabPanel>
-          <PagesWorkPricingChillerAC />
+        <TabPanel v-for="acOption in acOptions" :key="acOption.id">
+          <PagesWorkPricingACTabDetail :ac-data="pricingLists[active]" />
         </TabPanel>
       </TabView>
     </div>
+    <PagesWorkPricingTermsConditions />
   </div>
 </template>
 
