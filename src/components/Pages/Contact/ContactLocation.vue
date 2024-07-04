e585a9b331dfc015007b075c61d38ca1b4baad86
hasan75 <hasanjab14@gmail.com>
2024-07-05 01:45:09 +0600

    :children_crossing: Map location updated

--- src/components/Pages/Contact/ContactLocation.vue
+++ src/components/Pages/Contact/ContactLocation.vue
@@ -3,7 +3,7 @@
     <iframe
       class="border-none border-round-lg w-full h-21rem lg:h-27rem"
       height=""
-      src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d58395.533959751694!2d90.37678934162768!3d23.828522775792607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s%20BTI%20Premier%20Plaza%2C%20Shop%20%23%202%2FA%2C%202nd%20Floor%2C%20Cha%20%23%2090%2FA%2C%20Progoti%20Shoroni%2C%20shahjadpur%2C%20Gulshan%2C%20Badda%2C%20Dhaka.!5e0!3m2!1sen!2sbd!4v1696510625163!5m2!1sen!2sbd"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.2777569945583!2d90.41570348556203!3d23.773121532882353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c785158fae7b%3A0xdc528e731524c896!2sAC%20Service%20BD!5e0!3m2!1sen!2sbd!4v1720122241303!5m2!1sen!2sbd"
       loading="lazy"
       referrerpolicy="no-referrer-when-downgrade"
     />
