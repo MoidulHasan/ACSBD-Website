95f5ec6e617eda0a8bd4b640bc82aa3210feb62f
MoidulHasan <antu.khan.988@gmail.com>
2024-08-12 00:46:16 +0600

    refactor(frontend): Product large card refactored

--- src/components/Common/ProductCardLarge.vue
+++ src/components/Common/ProductCardLarge.vue
@@ -1,35 +1,76 @@
+<script lang="ts" setup>
+import type { ProductMinimalI } from "~/contracts/api-contracts/ProductsInterfaces";
+
+interface IProps {
+  productData: ProductMinimalI;
+}
+
+const props = defineProps<IProps>();
+
+const productRating = ref(props.productData.avg_ratings);
+</script>
+
 <template>
   <div class="product-card">
-    <NuxtLink class="w-full" :to="`/products/${id}`">
+    <NuxtLink :to="`/products/${productData.slug}`">
       <div class="w-full grid">
         <div class="col-4">
-          <div class="h-full flex justify-content-center relative">
-            <img :alt="name" :src="images[0]" class="product-image" />
-            <div v-if="price.discountPercentage" class="discount-percentage">
-              {{ price.discountPercentage }}% Off
+          <div class="image-container">
+            <img
+              :alt="productData.name"
+              :src="productData.image"
+              class="product-image"
+            />
+
+            <div
+              v-if="
+                productData.price.base_price !== productData.price.final_price
+              "
+              class="discount-percentage"
+            >
+              {{ productData.price.discount_amount }}
+              {{ productData.price.is_percent ? "%" : "Tk" }} Off
             </div>
           </div>
         </div>
 
         <div class="col-8">
           <div class="product-info">
-            <h3 class="product-name">
-              {{ name }}
-            </h3>
-
             <Rating v-model="productRating" :cancel="false" readonly />
 
+            <h3 class="product-name">
+              {{ productData.name }}
+            </h3>
+
+            <div
+              class="short-description"
+              v-html="productData.short_description"
+            />
+
             <p class="product-price-container">
-              <span v-if="price.discounted" class="product-price">
-                ৳ {{ price.discounted }}
-              </span>
               <span
+                v-if="productData.price.discount_amount"
+                class="product-price"
+              >
+                ৳ {{ productData.price.final_price }}
+              </span>
+
+              <span
+                v-if="
+                  productData.price.base_price !== productData.price.final_price
+                "
                 :class="[
-                  { 'product-price': !price.discounted },
-                  { 'initial-price': price.discounted },
+                  {
+                    'text-medium-2 text-primary-color-envitect-sam-blue':
+                      !productData.price.discount_amount,
+                  },
+                  {
+                    'text-regular-4 text-dark-gray-40 line-through':
+                      productData.price.discount_amount,
+                  },
                 ]"
               >
-                {{ price.currency }} {{ price.regular }}
+                ৳ {{ productData.price.base_price }}
               </span>
             </p>
           </div>
@@ -38,14 +79,6 @@
     </NuxtLink>
   </div>
 </template>
-
-<script lang="ts" setup>
-import type { ProductI } from "~/contracts/api-contracts/ProductsInterfaces";
-
-const props = defineProps<ProductI>();
-
-const productRating = ref(props.ratings.average);
-</script>
 
 <style lang="scss" scoped>
 .product-card {
@@ -58,43 +91,65 @@ const productRating = ref(props.ratings.average);
 
   cursor: pointer;
 
-  .product-image {
-    max-width: 100%;
+  .image-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    height: 220px;
 
-    vertical-align: bottom;
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
-    transition: opacity 150ms linear;
-    user-select: none;
-  }
+    .product-image {
+      max-width: 100%;
+      max-height: 100%;
 
-  .discount-percentage {
-    position: absolute;
-    right: 12px;
-    top: 12px;
+      vertical-align: bottom;
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+      transition: opacity 150ms linear;
+      user-select: none;
+    }
 
-    padding: 2px 12px;
-    align-items: flex-start;
-    gap: 8px;
+    .discount-percentage {
+      position: absolute;
+      right: 12px;
+      top: 12px;
 
-    border-radius: 2px;
-    background: var(--navy-blue-80);
+      padding: 2px 12px;
+      align-items: flex-start;
+      gap: 8px;
 
-    color: var(--primary-color-white);
-    text-align: center;
+      border-radius: 2px;
+      background: var(--navy-blue-80);
 
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 20px;
-  }
+      color: var(--primary-color-white);
+      text-align: center;
 
-  .product-image:hover {
-    opacity: 0.8;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 20px;
+    }
+
+    .product-image:hover {
+      opacity: 0.8;
+    }
   }
 
   .product-info {
     padding: 12px;
+
+    .short-description {
+      color: #565656;
+      font-size: 16px;
+      font-weight: 400;
+
+      width: 100%;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-line-clamp: 4;
+      -webkit-box-orient: vertical;
+    }
 
     :deep(.p-rating-icon) {
       color: #f2994a;
@@ -106,7 +161,7 @@ const productRating = ref(props.ratings.average);
 
       font-size: 16px;
       font-style: normal;
-      font-weight: 400;
+      font-weight: 600;
       line-height: 24px; /* 150% */
     }
 
