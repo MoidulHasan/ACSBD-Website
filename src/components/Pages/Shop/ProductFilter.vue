0de0a0e6623ff5e3814bf3a0a4c43c1954279dbe
MoidulHasan <antu.khan.988@gmail.com>
2024-08-13 03:11:26 +0600

    feat(frontend): product filtering initiated

--- src/components/Pages/Shop/ProductFilter.vue
+++ src/components/Pages/Shop/ProductFilter.vue
@@ -1,7 +1,25 @@
+<script lang="ts" setup>
+import type {
+  Category,
+  CategoryDataResponse,
+} from "~/contracts/api-contracts/categoryInterface";
+
+const { $apiClient } = useNuxtApp();
+
+const { data: productsCategories } = await useAsyncData<
+  CategoryDataResponse,
+  unknown,
+  Category[]
+>(`product-categories`, () => $apiClient(`/categories`), {
+  transform: (response) => response.data,
+});
+</script>
+
 <template>
   <div>filters</div>
+  <div>
+    {{ productsCategories }}
+  </div>
 </template>
-
-<script lang="ts" setup></script>
 
 <style scoped></style>
