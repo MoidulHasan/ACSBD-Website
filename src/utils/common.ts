6e789790888ba95f763e4d2124452c71d18c3714
MoidulHasan <antu.khan.988@gmail.com>
2024-08-16 04:28:42 +0600

    refactor(frontend): filter value collecting and findPriceRange util created

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,5 @@
 import type { FilterItem } from "~/contracts/common";
+import type { ProductMinimalI } from "~/contracts/api-contracts/ProductsInterfaces";
 
 interface ItemInterface {
   id: number | string;
@@ -8,13 +9,47 @@ interface ItemInterface {
   [key: string]: any;
 }
 
-export default function generateFilterItems(
-  items: ItemInterface[],
-): FilterItem[] {
+export function generateFilterItems(items: ItemInterface[]): FilterItem[] {
   return items.map(({ id, name, childrens }) => ({
     id,
     name,
     value: [],
     children: generateFilterItems(childrens),
   }));
+}
+
+export function collectAllValues(items: FilterItem[]): (number | string)[] {
+  return items.reduce<(number | string)[]>((acc, { value, children }) => {
+    // Collect the values from the current item
+    const currentValues = value;
+
+    // Recursively collect values from the children
+    const childValues = collectAllValues(children);
+
+    // Concatenate the current values and child values to the accumulator
+    return acc.concat(currentValues, childValues);
+  }, []);
+}
+
+export function findPriceRange(products: ProductMinimalI[]): [number, number] {
+  if (!products.length) {
+    return [0, 0]; // Return a default value if the array is empty
+  }
+
+  let lowerPrice = Infinity;
+  let higherPrice = -Infinity;
+
+  products.forEach((product) => {
+    const finalPrice = parseFloat(product.price.final_price);
+
+    if (finalPrice < lowerPrice) {
+      lowerPrice = finalPrice;
+    }
+
+    if (finalPrice > higherPrice) {
+      higherPrice = finalPrice;
+    }
+  });
+
+  return [lowerPrice, higherPrice];
 }
