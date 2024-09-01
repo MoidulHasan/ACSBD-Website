1cee2250168f2636edbb90294cb30aaaa7f7472a
hasan75 <hasanjab14@gmail.com>
2024-09-01 14:50:58 +0600

    :sparkles: Faq api integrated on website

--- src/components/Pages/Faq/TopTitle.vue
+++ src/components/Pages/Faq/TopTitle.vue
@@ -1,19 +1,31 @@
+<script lang="ts" setup>
+const emit = defineEmits<{
+  (e: "search", payload: string): void;
+}>();
+const searchQuestion = ref("");
+</script>
+
 <template>
   <div class="flex flex-column align-items-center text-center">
     <h1 class="font-heading-2 text-primary-color-navy-blue mb-24px">
       Frequently Asked Questions
     </h1>
     <div class="p-inputgroup searchbar">
-      <InputText class="py-12px pl-24px" placeholder="Search FAQs Here" />
+      <InputText
+        v-model="searchQuestion"
+        type="text"
+        class="py-12px pl-24px"
+        placeholder="Search FAQs Here"
+        @update:model-value="emit('search', searchQuestion)"
+      />
       <Button
         class="px-32px py-12px bg-primary-color-envitect-sam-blue"
         icon="pi pi-search"
+        @click="emit('search', searchQuestion)"
       />
     </div>
   </div>
 </template>
-
-<script lang="ts" setup></script>
 
 <style lang="scss" scoped>
 @use "assets/styles/scss/base/_mixins.scss" as *;
--- src/pages/frequently-asked-questions.vue
+++ src/pages/frequently-asked-questions.vue
@@ -1,30 +1,7 @@
-<template>
-  <div class="flex flex-column align-items-center">
-    <div class="container">
-      <PagesFaqTopTitle class="my-48px" />
-    </div>
-
-    <PagesFaqTabsSection
-      :active-tab-index="activeTabIndex"
-      :faq-tabs="faqTabData"
-      :style="{ zIndex: 2 }"
-      class="mb-48px py-5"
-      @on-active-tab-change="handleActiveTabChange"
-    />
-
-    <div class="faq-container">
-      <PagesFaqTabComponent :faq-list="faqListOfSelectedTab" />
-    </div>
-
-    <div class="more-question-form-container mt-48px mb-80px">
-      <PagesFaqMoreQuestionForm class="w-full" />
-    </div>
-  </div>
-</template>
-
 <script lang="ts" setup>
+import { useStorage } from "@vueuse/core";
 import { getFAQData } from "~/app/api/getFAQData";
-import type { FAQSection } from "~/contracts/api-contracts/faqData";
+import type { FAQSection, FAQTab } from "~/contracts/api-contracts/faqData";
 
 definePageMeta({
   title: "Frequently Asked Questions",
@@ -33,27 +10,123 @@ definePageMeta({
 
 const { data: allFaqData } = await getFAQData();
 
-const faqTabData = computed(() => {
-  if (!allFaqData.value) return [];
+const { $apiClient } = useNuxtApp();
 
-  return allFaqData.value.map((faqData: FAQSection) => {
-    return {
-      title: faqData.title,
-      image: faqData.image,
-      description: faqData.description,
-    };
-  });
-});
+const { data: allFaqs } = await useAsyncData("frequently-asked-questions", () =>
+  $apiClient(`/faqs`),
+);
+
+const faqTabs: Omit<FAQSection, "tabs">[] = [
+  {
+    title: "Services",
+    slug: "service",
+    image: "/images/faq/services.svg",
+    description:
+      "We have a large team to service any kind of branded AC with the latest tools under experienced engineer. Best AC servicing company in Bangladesh.",
+  },
+  {
+    title: "AC Rent",
+    slug: "ac_rent",
+    image: "/images/faq/ac_rent.svg",
+    description:
+      "We have a large team to service any kind of branded AC with the latest tools under experienced engineer. Best AC servicing company in Bangladesh.",
+  },
+  {
+    title: "Products",
+    slug: "product",
+    image: "/images/faq/products.svg",
+    description:
+      "We have a large team to service any kind of branded AC with the latest tools under experienced engineer. Best AC servicing company in Bangladesh.",
+  },
+  {
+    title: "Policy",
+    slug: "policy",
+    image: "/images/faq/policy.svg",
+    description:
+      "We have a large team to service any kind of branded AC with the latest tools under experienced engineer. Best AC servicing company in Bangladesh.",
+  },
+];
+const activeTabIndex = ref(0);
+const activeTabName = useStorage("activeTabName", "service");
+const faqSearchText = ref("");
+
+const handleActiveTabChange = (index: number, name: string) => {
+  activeTabName.value = name;
+  activeTabIndex.value = index;
+};
+
+console.log(allFaqs.value, "ALLFAQS");
+
+// const faqTabData = computed(() => {
+//   if (!allFaqData.value) return [];
+//
+//   return allFaqData.value.map((faqData: FAQSection) => {
+//     return {
+//       title: faqData.title,
+//       image: faqData.image,
+//       description: faqData.description,
+//     };
+//   });
+// });
 
 const faqListOfSelectedTab = computed(() => {
   return allFaqData.value?.length ? allFaqData.value[activeTabIndex.value] : [];
 });
+console.log(faqListOfSelectedTab.value, "PREVIOUS");
 
-const activeTabIndex = ref(0);
-const handleActiveTabChange = (index: number) => {
-  activeTabIndex.value = index;
-};
+const selectedFaqList = computed(() => {
+  if (allFaqs.value?.data && activeTabName.value) {
+    const faqs = allFaqs.value.data[activeTabName.value];
+
+    if (faqs) {
+      return faqSearchText.value
+        ? faqs.filter((item: FAQTab) =>
+            item.question
+              .toLowerCase()
+              .includes(faqSearchText.value.toLowerCase()),
+          )
+        : faqs;
+    }
+  }
+  return [];
+});
+// activeTabIndex;
+console.log(selectedFaqList.value, "SELECTED FAQ LIST", activeTabName.value);
+
+const selectedFaqTab = computed(() => {
+  return faqTabs.find((faq) => {
+    return faq.slug === activeTabName.value;
+  });
+});
+console.log(selectedFaqTab.value, "SELECTED FAW");
 </script>
+
+<template>
+  <div class="flex flex-column align-items-center">
+    <div class="container">
+      <PagesFaqTopTitle class="my-48px" @search="faqSearchText = $event" />
+    </div>
+
+    <PagesFaqTabsSection
+      :faq-tabs="faqTabs"
+      :active-tab-index="activeTabIndex"
+      :style="{ zIndex: 2 }"
+      class="mb-48px py-5"
+      @on-active-tab-change="handleActiveTabChange"
+    />
+
+    <div class="faq-container">
+      <PagesFaqTabComponent
+        :faq-list="selectedFaqList"
+        :selected-faq-tab="selectedFaqTab"
+      />
+    </div>
+
+    <div class="more-question-form-container mt-48px mb-80px">
+      <PagesFaqMoreQuestionForm class="w-full" />
+    </div>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 .faq-container {
