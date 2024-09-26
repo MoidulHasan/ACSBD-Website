147048ca977128c0c6c8a80169eb67f88daa1879
MoidulHasan <antu.khan.988@gmail.com>
2024-09-27 00:06:58 +0600

    refactor(frontend): service sections design refactored

--- src/app/api/services.ts
+++
@@ -1,9 +0,0 @@
-export const getServices = async () => {
-  const runtimeConfig = useRuntimeConfig();
-
-  const { data, pending, error, refresh } = await useFetch(
-    runtimeConfig.public.apiUrl + "/data/services.json",
-  );
-
-  return { data, pending, error, refresh };
-};
