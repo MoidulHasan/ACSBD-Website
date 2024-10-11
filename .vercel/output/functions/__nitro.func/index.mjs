315c1ac3f945010785c84b71fddba6fce9a8c380
MoidulHasan <antu.khan.988@gmail.com>
2024-10-12 03:20:46 +0600

    refactor(frontend): vercel build removed

--- .vercel/output/functions/__nitro.func/index.mjs
+++
@@ -1,9 +0,0 @@
-import process from 'node:process';globalThis._importMeta_={url:import.meta.url,env:process.env};export { O as default } from './chunks/runtime.mjs';
-import 'node:http';
-import 'node:https';
-import 'node:fs';
-import 'node:path';
-import 'vue';
-import 'node:url';
-import 'ipx';
-//# sourceMappingURL=index.mjs.map
